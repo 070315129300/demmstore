e2950410fd65fc91559517770bce2df2717bc1d9
k-leb <ikhidecaleb@gmail.com>
2023-03-11 03:33:20 +0100

    my first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demmstore</title>
+{{--    <link rel="stylesheet" href="{{url('style.style')}}">--}}
+    <link rel="stylesheet" href="{{url('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{url('style/styles.css')}}">
+
+
+
+
+</head>
+<body>
+<div id="header">
+
+
+<div class="container" >
+
+
+ <div class="navbar">
+     <div class="log">
+         <img src="img/logo.jpg" alt="" width="100px">
+     </div>
+     <nav>
+         <ul>
+             <li><a href="">Home</a></li>
+             <li><a href="">Products</a></li>
+             <li><a href="">About</a></li>
+             <li><a href="">Contact</a></li>
+             <li><a href="">Account</a></li>
+             <li><img src="img/picture2.png" alt="" width="30px" height="30px"></li>
+         </ul>
+     </nav>
+
+ </div>
+
+{{--    end of navbar--}}
+    <div class="row">
+        <div class="col-6 " style="padding-top: 150px">
+          <h1>Give Your Workout <br>A New Style!</h1>
+            <p>Success isn't always about greatness. It's about consistency. </p>
+            <a href="" class="  btn btn-outline-warning"> Explore Now &#8594;</a>
+        </div>
+        <div class="col-6">
+            <img src="img/picture1.jpg" alt="" style="padding: 50px">
+        </div>
+    </div>
+</div>
+</div>
+{{--end of header--}}
+<div >
+
+</div>
+</body>
+</html>
+
